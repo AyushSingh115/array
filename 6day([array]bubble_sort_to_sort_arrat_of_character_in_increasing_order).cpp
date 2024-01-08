27af4b7453753a723f6c6debc71092f42a85d6c3
AyushSingh115 <148227081+AyushSingh115@users.noreply.github.com>
2024-01-08 11:52:19 +0530

    Add files via upload

---
+++ 6day([array]bubble_sort_to_sort_arrat_of_character_in_increasing_order).cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char n;
+    char arr[1000];
+    cout<<"enter the size the array";
+    cin>>n;
+    for(char i='a';i<n;i++)
+    {
+        cout<<" enter the size";
+        cin>>arr[i];
+    }
+    for(char i=n-2;i>='a';i--)
+    {
+        bool swapped=0;
+        for(char j='a';j<=i;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swapped=1;
+                swap(arr[j],arr[j+1]);
+            }
+        }
+        if (swapped==0)
+            break;
+    }
+    for(char i='a';i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
